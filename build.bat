a7f48a022428e7f5a874357776f44cd4644052d5
tdrelangue <tdrelangue@ensc.fr>
2025-12-04 16:16:09 +0100

    build

--- build.bat
+++ build.bat
@@ -1,9 +1,13 @@
 @echo off
 REM Compilation de l'application APA_Email en .exe
+python -m pip install -r requirements.txt
 python -m PyInstaller -F -w -n "Rapporteur ASH" --icon "assets/icon.ico" --add-data "assets;assets" ASH_Email.py
 
 REM Copie des fichiers de configuration modifiables
 xcopy templates dist\templates\ /E /I /Y
+xcopy assets dist\assets\ /E /I /Y
+xcopy Protégés dist\Protégés\ /E /I /Y
+xcopy .env dist\ /E /I /Y
 
 echo.
 echo === Build terminé ===
